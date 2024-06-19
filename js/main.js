208ff0dcbb89fbc90067a9a7df0d353c93aeeea5
조범준 <oreasv3@gmail.com>
2024-06-19 17:36:40 +0900

    포트폴리오 설정

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let greeting = 'Hello, world!';
+
+console.log(greetting);
+
+let nickname = '짐코딩';
+let hobby = ['헬스', '수영'];
+
+let person = {
+  nickname: nickname,
+  hobby: hobby,
+};
