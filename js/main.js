224b1b3ba97ddd7f6be1517f9cad4952ec1212a3
조범준 <oreasv3@gmail.com>
2024-06-27 18:41:12 +0900

    progessBar 뷰포트 진입시 애니메이션 다같이 실행

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const progessBars = document.querySelectorAll('.skills__progessbar');
-
   // 프로젝트 클릭시 세부 정보 노출
   function init() {
     const $projectsLink = document.getElementById('projects__link');
@@ -14,26 +12,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
   init();
 
-  // Skills 섹션의 progessBar가 뷰포트 진입시 애니메이션 시작위한 코드
+  // Skills 섹션의 progessBar가 뷰포트 진입시 애니메이션 시작 위한 코드
+
+  // progessBar들의 모임을 가져옴
+  const progessCollections = document.querySelectorAll(
+    '.skills__progess-collection',
+  );
+
   function handleIntersection(entries, observe) {
     entries.forEach((entry) => {
-      // 각 요소가 뷰포트에 들어왔다면
+      // 요소가 뷰포트에 들어왔다면
       if (entry.isIntersecting) {
-        const progessBar = entry.target;
-        const percentage = progessBar.querySelector('.skills__percentage');
-        // skills__percentage-text에 문자열을 가져옴
-        const text = percentage.querySelector(
-          '.skills__percentage-text',
-        ).textContent;
-
-        //숫자만 추출
-        const value = parseInt(text, 10);
-
-        // width를 결정
-        percentage.style.width = value - 1 + '%';
-
+        const container = entry.target;
+        const percentages = container.querySelectorAll('.skills__percentage');
+        percentages.forEach((bar) => {
+          // skills__percentage-text에 문자열을 가져옴
+          const text = bar.querySelector(
+            '.skills__percentage-text',
+          ).textContent;
+          //숫자만 추출
+          const value = parseInt(text, 10);
+          // width를 결정
+          bar.style.width = value - 1 + '%';
+        });
         // 한번 실행후 다시 실행 안되도록
-        observer.unobserve(progessBar);
+        observer.unobserve(container);
       }
     });
   }
@@ -44,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // 변화 감지할 요소를 등록
-  progessBars.forEach((bar) => {
-    observer.observe(bar);
+  progessCollections.forEach((container) => {
+    observer.observe(container);
   });
 });
